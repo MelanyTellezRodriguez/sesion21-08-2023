1d55859f37ca3611e6bc7fbed4a640641f21bdf1
MelanyTellezRodriguez <melanytell1101@gmail.com>
2023-09-08 21:32:53 -0600

    Se creo el proyecto MiPrimerProgramaJava

---
+++ MiPrimerPrograma.java/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String MiNombre = "Melany Tellez";
+        int edad = 10;
+        float salarioBruto;
+        System.out.println(MiNombre);
+        System.out.println(edad);
+
+    }
+}
